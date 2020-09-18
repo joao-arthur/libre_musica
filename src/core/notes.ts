f5678a0dbc6ea6c7135680cd9241a9b81f06899a
Joao-Arthur <joao.lothamer@gmail.com>
2020-09-18 20:53:35 -0300

    implements new settings

--- src/core/notes.ts
+++ src/core/notes.ts
@@ -44,50 +44,68 @@ const modes = [
 export const getScale = (
     note: number = 0,
     modeIndex: number = 0,
-    scaleKind: number[] = diatonicScale
+    scaleKind: number[] = scales.diatonic
 ) =>
     reorderArrayByIndex(scaleKind, modeIndex).map(
         noteOfScale => (noteOfScale + note) % 12
     );
 
 const test = () => {
-    console.log(getScale(0, 0, diatonicScale).map(e => notesArray[e]));
-    console.log(getScale(0, 1, diatonicScale).map(e => notesArray[e]));
-    console.log(getScale(2, 0, diatonicScale).map(e => notesArray[e]));
+    console.log(getScale(0, 0, scales.diatonic).map(e => notesArray[e]));
+    console.log(getScale(0, 1, scales.diatonic).map(e => notesArray[e]));
+    console.log(getScale(2, 0, scales.diatonic).map(e => notesArray[e]));
 };
 
 //const melodicScale = [];
 
 export const strings = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const dropd = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.D];
-const dropc = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const bass = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const dropdbass = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const dropcbass = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const bass5 = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const bass6 = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const guitar7 = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const guitar8 = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
-const dadgad = [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E];
 
 const tunings = {
     guitar: {
-        6: { standard: [], dropd: [], dropc: [], dadgad: [] },
-        7: { standard: [] },
-        8: { standard: [] }
+        6: {
+            standard: [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E],
+            dropd: [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.D],
+            dropc: [Notes.D, Notes.A, Notes.F, Notes.C, Notes.G, Notes.C],
+            dadgad: [Notes.D, Notes.A, Notes.G, Notes.D, Notes.A, Notes.D]
+        },
+        7: {
+            standard: [
+                Notes.E,
+                Notes.B,
+                Notes.G,
+                Notes.D,
+                Notes.A,
+                Notes.E,
+                Notes.B
+            ]
+        }
     },
     bass: {
-        4: { standard: [], dropd: [], dropc: [] },
-        5: { standard: [] },
-        6: { standard: [] }
+        4: {
+            standard: [Notes.G, Notes.D, Notes.A, Notes.E],
+            dropd: [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E],
+            dropc: [Notes.E, Notes.B, Notes.G, Notes.D, Notes.A, Notes.E]
+        },
+        5: { standard: [Notes.G, Notes.D, Notes.A, Notes.E, Notes.B] },
+        6: {
+            standard: [
+                Notes.C,
+                Notes.B,
+                Notes.G,
+                Notes.D,
+                Notes.A,
+                Notes.E,
+                Notes.B
+            ]
+        }
     }
 };
 
 const scales = {
-    diatonic = [0, 2, 4, 5, 7, 9, 11],
-    harmonic = [0, 2, 4, 5, 7, 8, 11],
-    doubleHarmonic = [0, 1, 4, 5, 7, 8, 11],
-    pentatonic = [0, 2, 4, 7, 9]
+    diatonic: [0, 2, 4, 5, 7, 9, 11],
+    harmonic: [0, 2, 4, 5, 7, 8, 11],
+    doubleHarmonic: [0, 1, 4, 5, 7, 8, 11],
+    pentatonic: [0, 2, 4, 7, 9]
 };
 
 const settings = {
@@ -95,7 +113,7 @@ const settings = {
         tunings: {
             6: ['standard', 'dropd', 'dropc', 'dadgad'],
             7: ['standard'],
-            7: ['standard']
+            8: ['standard']
         },
         scales: ['diatonic', 'harmonic', 'doubleHarmonic', 'pentatonic'],
         modes: {
