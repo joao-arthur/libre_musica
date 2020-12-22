2a06261d9618795ef82c69af77123615093bd0f8
Joao-Arthur <joao.lothamer@gmail.com>
2020-12-22 18:52:22 -0300

    atualiza npm

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
