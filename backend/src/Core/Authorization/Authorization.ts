6266a7481051b994c1f30d834f732802c220464c
Joao-Arthur <joao.lothamer@gmail.com>
2021-02-11 21:30:49 -0300

    fix authorization encryption

--- backend/src/Core/Authorization/Authorization.ts
+++ backend/src/Core/Authorization/Authorization.ts
@@ -1,5 +1,5 @@
 import * as RandomString from 'crypto-random-string';
-import NodeRSA from 'node-rsa';
+import * as NodeRSA from 'node-rsa';
 import * as fs from 'fs';
 
 const generateSalt = () =>
@@ -8,13 +8,13 @@ const generateSalt = () =>
 const getIterations = () => 1;
 
 const encryptRSA = (text: string) => {
-    const data = fs.readFileSync('Keys/public.der');
+    const data = fs.readFileSync(`${__dirname}/Keys/public.der`);
     const publicKey = new NodeRSA(data, 'pkcs8-public-der');
     return publicKey.encrypt(text, 'base64');
 };
 
 const decryptRSA = (text: string) => {
-    const data = fs.readFileSync('Keys/private.der');
+    const data = fs.readFileSync(`${__dirname}/Keys/private.der`);
     const privateKey = new NodeRSA(data, 'pkcs8-private-der');
     return privateKey.decrypt(text, 'utf8');
 };
