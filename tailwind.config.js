d832284159083dc47bda55047232b175718266ec
Joao-Arthur <joao.lothamer@gmail.com>
2022-05-20 12:17:55 -0300

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,24 @@ module.exports = {
     darkMode: 'class',
     theme: {
         extend: {
-            colors: {    
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                primary: {
+                    light: '#63a4ff',
+                    DEFAULT: '#1976d2',
+                    dark: '#004ba0'
+                },
+                grey: {
+                    light: '#f5f5f5',
+                    DEFAULT: '#c2c2c2',
+                    dark: ' #929292'
+                }
             },
             flex: {
                 '0-auto': '0 0 auto',
-                0: '0 1 0%'
-            },
-            margin: {
-                '-100': '-100%'
-            },
-            width: {
-                '1/10': '10%',
-                100: '400px',
-                120: '500px'
-            },
-            maxWidth: {
-                '4/5': '80%'
+                0: '0 1 0%',
+                '1-0-auto': '1 0 auto'
             }
         }
     },
