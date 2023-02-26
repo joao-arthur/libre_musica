ab3fa3300fbb6d75943b3128c5823cc65e959033
joao-arthur <joao.lothamer@gmail.com>
2023-02-26 17:16:40 -0300

    fix: remove target

--- backend/target/debug/deps/musica-184c4c66dd69e366.d
+++
@@ -1,5 +0,0 @@
-/home/joao/Documents/programming/Musica/musica/target/debug/deps/musica-184c4c66dd69e366.rmeta: src/main.rs
-
-/home/joao/Documents/programming/Musica/musica/target/debug/deps/musica-184c4c66dd69e366.d: src/main.rs
-
-src/main.rs:
