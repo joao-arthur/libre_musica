a2af102b72a38ba564165c203ea82ecdb826c3e9
joao-arthur <joao.lothamer@gmail.com>
2023-02-26 16:51:24 -0300

    refactor: move project

---
+++ backend/target/debug/deps/musica-2f86c58e94ffa660.d
@@ -0,0 +1,5 @@
+/home/joao/Documents/programming/Musica/musica/target/debug/deps/musica-2f86c58e94ffa660.rmeta: src/main.rs
+
+/home/joao/Documents/programming/Musica/musica/target/debug/deps/musica-2f86c58e94ffa660.d: src/main.rs
+
+src/main.rs:
