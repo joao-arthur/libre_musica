292e241560073fbab16868ce3dfb5055a29a1afc
joao-arthur <joao.lothamer@gmail.com>
2024-04-04 14:26:05 -0300

    wip: vite to next

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,38 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    mode: 'jit',
-    purge: ['./src/**/*.tsx'],
-    darkMode: 'class',
+    content: [
+        './src/**/*.tsx',
+    ],
     theme: {
-        extend: {
-            colors: {
-                transparent: 'transparent',
-                current: 'currentColor',
-                primary: {
-                    light: '#63a4ff',
-                    DEFAULT: '#1976d2',
-                    dark: '#004ba0'
-                },
-                grey: {
-                    light: '#f5f5f5',
-                    DEFAULT: '#c2c2c2',
-                    dark: ' #929292'
-                }
-            },
-            flex: {
-                '0-auto': '0 0 auto',
-                0: '0 1 0%',
-                '1-0-auto': '1 0 auto'
-            }
-        }
+        extend: {},
     },
-    variants: {
-        extend: {
-            backgroundColor: ['active'],
-            border: ['active'],
-            borderColor: ['active'],
-            textColor: ['active'],
-            fontSize: ['active']
-        }
-    },
-    plugins: []
+    plugins: [
+        require('@tailwindcss/typography'),
+    ],
 };
