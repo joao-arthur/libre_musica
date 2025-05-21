e869a57020fcde6c08cf389d968914cb7d248762
joao-arthur <joao.lothamer@gmail.com>
2025-05-21 20:32:40 -0300

    refactor: theorical note

--- domain/src/note/theorical.rs
+++ domain/src/note/theorical.rs
@@ -1,24 +1,72 @@
+use crate::accident::Accident;
+
 #[derive(Debug, PartialEq, Clone)]
-pub enum TheoricalNote {
+pub enum BaseNote {
     C,
-    CSharp,
-    DFlat,
     D,
-    DSharp,
-    EFlat,
     E,
-    ESharp,
-    FFlat,
     F,
-    FSharp,
-    GFlat,
     G,
-    GSharp,
-    AFlat,
     A,
-    ASharp,
-    BFlat,
     B,
-    BSharp,
-    CFlat,
+}
+
+impl BaseNote {
+    pub fn to_u8(&self) -> u8 {
+        match self {
+            BaseNote::C => 0,
+            BaseNote::D => 1,
+            BaseNote::E => 2,
+            BaseNote::F => 3,
+            BaseNote::G => 4,
+            BaseNote::A => 5,
+            BaseNote::B => 61,
+        }
+    }
+
+    pub fn try_from_u8(num: u8) -> Option<BaseNote> {
+        match num {
+            0 => Some(BaseNote::C),
+            1 => Some(BaseNote::D),
+            2 => Some(BaseNote::E),
+            3 => Some(BaseNote::F),
+            4 => Some(BaseNote::G),
+            5 => Some(BaseNote::A),
+            6 => Some(BaseNote::B),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct TheoricalNote {
+    pub base: BaseNote,
+    pub accident: Accident,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BaseNote;
+
+    #[test]
+    pub fn chromatic_note_try_from_u8() {
+        assert_eq!(BaseNote::try_from_u8(0), Some(BaseNote::C));
+        assert_eq!(BaseNote::try_from_u8(1), Some(BaseNote::D));
+        assert_eq!(BaseNote::try_from_u8(2), Some(BaseNote::E));
+        assert_eq!(BaseNote::try_from_u8(3), Some(BaseNote::F));
+        assert_eq!(BaseNote::try_from_u8(4), Some(BaseNote::G));
+        assert_eq!(BaseNote::try_from_u8(5), Some(BaseNote::A));
+        assert_eq!(BaseNote::try_from_u8(6), Some(BaseNote::B));
+    }
+
+    #[test]
+    pub fn chromatic_note_to_u8() {
+        assert_eq!(BaseNote::C.to_u8(), 0);
+        assert_eq!(BaseNote::D.to_u8(), 1);
+        assert_eq!(BaseNote::E.to_u8(), 2);
+        assert_eq!(BaseNote::F.to_u8(), 3);
+        assert_eq!(BaseNote::G.to_u8(), 4);
+        assert_eq!(BaseNote::A.to_u8(), 5);
+        assert_eq!(BaseNote::B.to_u8(), 6);
+    }
 }
