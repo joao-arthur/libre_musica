92756822cd4b21d726d045a44e7105d4f261dc71
joao-arthur <joao.lothamer@gmail.com>
2025-05-21 20:49:49 -0300

    feat: vector of notes helper functions

--- domain/src/note/theorical.rs
+++ domain/src/note/theorical.rs
@@ -44,9 +44,17 @@ pub struct TheoricalNote {
     pub accident: Accident,
 }
 
+pub fn from_vec(vec: Vec<u8>) -> Vec<BaseNote> {
+    vec.iter().map(|num| BaseNote::try_from_u8(*num)).filter_map(|num| num).collect()
+}
+
+pub fn from_slice<const N: usize>(slice: [u8; N]) -> Vec<BaseNote> {
+    slice.to_vec().iter().map(|num| BaseNote::try_from_u8(*num)).filter_map(|num| num).collect()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::BaseNote;
+    use super::{BaseNote, from_vec, from_slice};
 
     #[test]
     pub fn chromatic_note_try_from_u8() {
@@ -68,5 +76,23 @@ mod tests {
         assert_eq!(BaseNote::G.to_u8(), 4);
         assert_eq!(BaseNote::A.to_u8(), 5);
         assert_eq!(BaseNote::B.to_u8(), 6);
+    }
+
+    #[test]
+    fn test_from_vec() {
+        assert_eq!(from_vec(vec![0, 1]), vec![BaseNote::C, BaseNote::D]);
+        assert_eq!(from_vec(vec![2, 3]), vec![BaseNote::E, BaseNote::F]);
+        assert_eq!(from_vec(vec![4, 5]), vec![BaseNote::G, BaseNote::A]);
+        assert_eq!(from_vec(vec![6, 7]), vec![BaseNote::B]);
+        assert_eq!(from_vec(vec![8, 9]), vec![]);
+    }
+
+    #[test]
+    fn test_from_slice() {
+        assert_eq!(from_slice([0, 1]), vec![BaseNote::C, BaseNote::D]);
+        assert_eq!(from_slice([2, 3]), vec![BaseNote::E, BaseNote::F]);
+        assert_eq!(from_slice([4, 5]), vec![BaseNote::G, BaseNote::A]);
+        assert_eq!(from_slice([6, 7]), vec![BaseNote::B]);
+        assert_eq!(from_slice([8, 9]), vec![]);
     }
 }
