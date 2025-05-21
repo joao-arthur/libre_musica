38bb50b291b4cfbd59af90ee4c3f5dc954c934e5
joao-arthur <joao.lothamer@gmail.com>
2025-05-21 19:37:29 -0300

    refactor: accident

--- domain/src/accident.rs
+++ domain/src/accident.rs
@@ -1,5 +1,34 @@
+#[derive(Debug, PartialEq, Clone)]
 pub enum Accident {
-    Natural,
+    DoubleFlat,
     Flat,
+    Natural,
     Sharp,
+    DoubleSharp,
+}
+
+impl Accident {
+    pub fn to_u8(&self) -> i8 {
+        match self {
+            Accident::DoubleFlat => -2,
+            Accident::Flat => -1,
+            Accident::Natural => 0,
+            Accident::Sharp => 1,
+            Accident::DoubleSharp => 2,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Accident;
+
+    #[test]
+    fn accident_to_u8() {
+        assert_eq!(Accident::DoubleFlat.to_u8(), -2);
+        assert_eq!(Accident::Flat.to_u8(), -1);
+        assert_eq!(Accident::Natural.to_u8(), 0);
+        assert_eq!(Accident::Sharp.to_u8(), 1);
+        assert_eq!(Accident::DoubleSharp.to_u8(), 2);
+    }
 }
