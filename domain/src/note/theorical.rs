f976959a661a7544cc351e50fdffac4b71cc3665
joao-arthur <joao.lothamer@gmail.com>
2025-05-22 21:11:07 -0300

    feat: debug TheoricalNote

--- domain/src/note/theorical.rs
+++ domain/src/note/theorical.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::accident::Accident;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -42,6 +44,12 @@ impl BaseNote {
 pub struct TheoricalNote {
     pub base: BaseNote,
     pub accident: Accident,
+}
+
+impl fmt::Display for TheoricalNote {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_str())
+    }
 }
 
 impl TheoricalNote {
@@ -101,6 +109,52 @@ impl TheoricalNote {
 
     pub fn from_str(value: &str) -> TheoricalNote {
         Self::try_from_str(value).unwrap()
+    }
+
+    pub fn to_str(&self) -> String {
+        match self {
+            TheoricalNote { base: BaseNote::C, accident: Accident::DoubleFlat } => "C♭♭".into(),
+            TheoricalNote { base: BaseNote::C, accident: Accident::Flat } => "C♭".into(),
+            TheoricalNote { base: BaseNote::C, accident: Accident::Natural } => "C".into(),
+            TheoricalNote { base: BaseNote::C, accident: Accident::Sharp } => "C♯".into(),
+            TheoricalNote { base: BaseNote::C, accident: Accident::DoubleSharp } => "C♯♯".into(),
+
+            TheoricalNote { base: BaseNote::D, accident: Accident::DoubleFlat } => "D♭♭".into(),
+            TheoricalNote { base: BaseNote::D, accident: Accident::Flat } => "D♭".into(),
+            TheoricalNote { base: BaseNote::D, accident: Accident::Natural } => "D".into(),
+            TheoricalNote { base: BaseNote::D, accident: Accident::Sharp } => "D♯".into(),
+            TheoricalNote { base: BaseNote::D, accident: Accident::DoubleSharp } => "D♯♯".into(),
+
+            TheoricalNote { base: BaseNote::E, accident: Accident::DoubleFlat } => "E♭♭".into(),
+            TheoricalNote { base: BaseNote::E, accident: Accident::Flat } => "E♭".into(),
+            TheoricalNote { base: BaseNote::E, accident: Accident::Natural } => "E".into(),
+            TheoricalNote { base: BaseNote::E, accident: Accident::Sharp } => "E♯".into(),
+            TheoricalNote { base: BaseNote::E, accident: Accident::DoubleSharp } => "E♯♯".into(),
+
+            TheoricalNote { base: BaseNote::F, accident: Accident::DoubleFlat } => "F♭♭".into(),
+            TheoricalNote { base: BaseNote::F, accident: Accident::Flat } => "F♭".into(),
+            TheoricalNote { base: BaseNote::F, accident: Accident::Natural } => "F".into(),
+            TheoricalNote { base: BaseNote::F, accident: Accident::Sharp } => "F♯".into(),
+            TheoricalNote { base: BaseNote::F, accident: Accident::DoubleSharp } => "F♯♯".into(),
+
+            TheoricalNote { base: BaseNote::G, accident: Accident::DoubleFlat } => "G♭♭".into(),
+            TheoricalNote { base: BaseNote::G, accident: Accident::Flat } => "G♭".into(),
+            TheoricalNote { base: BaseNote::G, accident: Accident::Natural } => "G".into(),
+            TheoricalNote { base: BaseNote::G, accident: Accident::Sharp } => "G♯".into(),
+            TheoricalNote { base: BaseNote::G, accident: Accident::DoubleSharp } => "G♯♯".into(),
+
+            TheoricalNote { base: BaseNote::A, accident: Accident::DoubleFlat } => "A♭♭".into(),
+            TheoricalNote { base: BaseNote::A, accident: Accident::Flat } => "A♭".into(),
+            TheoricalNote { base: BaseNote::A, accident: Accident::Natural } => "A".into(),
+            TheoricalNote { base: BaseNote::A, accident: Accident::Sharp } => "A♯".into(),
+            TheoricalNote { base: BaseNote::A, accident: Accident::DoubleSharp } => "A♯♯".into(),
+
+            TheoricalNote { base: BaseNote::B, accident: Accident::DoubleFlat } => "B♭♭".into(),
+            TheoricalNote { base: BaseNote::B, accident: Accident::Flat } => "B♭".into(),
+            TheoricalNote { base: BaseNote::B, accident: Accident::Natural } => "B".into(),
+            TheoricalNote { base: BaseNote::B, accident: Accident::Sharp } => "B♯".into(),
+            TheoricalNote { base: BaseNote::B, accident: Accident::DoubleSharp } => "B♯♯".into(),
+        }
     }
 }
 
