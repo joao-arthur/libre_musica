e285bad438c3a7f4ac16e48dfa4e0d00cafe630b
joao-arthur <joao.lothamer@gmail.com>
2025-05-31 20:10:27 -0300

    test: theorical major scale

--- domain/src/scale/theorical/major.rs
+++ domain/src/scale/theorical/major.rs
@@ -16,24 +16,53 @@ mod tests {
     use super::major_scale_of;
 
     #[test]
-    fn build_major_scale_natural() {
-        assert_eq!(major_scale_of(TheoricalNote::from_str("D")), vec_of_slice_str(["D", "E", "F♯", "G", "A", "B", "C♯"]));
-        assert_eq!(major_scale_of(TheoricalNote::from_str("E")), vec_of_slice_str(["E", "F♯", "G♯", "A", "B", "C♯", "D♯"]));
-        assert_eq!(major_scale_of(TheoricalNote::from_str("F")), vec_of_slice_str(["F", "G", "A", "B♭", "C", "D", "E"]));
-        assert_eq!(major_scale_of(TheoricalNote::from_str("G")), vec_of_slice_str(["G", "A", "B", "C", "D", "E", "F♯"]));
-        assert_eq!(major_scale_of(TheoricalNote::from_str("A")), vec_of_slice_str(["A", "B", "C♯", "D", "E", "F♯", "G♯"]));
-        assert_eq!(major_scale_of(TheoricalNote::from_str("B")), vec_of_slice_str(["B", "C♯", "D♯", "E", "F♯", "G♯", "A♯"]));
-    }
-
-    #[test]
     fn test_build_major_scale_c() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("C𝄫")), vec_of_slice_str(["C𝄫", "D𝄫", "E𝄫", "F𝄫", "G𝄫", "A𝄫", "B𝄫"]));
         assert_eq!(major_scale_of(TheoricalNote::from_str("C♭")), vec_of_slice_str(["C♭", "D♭", "E♭", "F♭", "G♭", "A♭", "B♭"]));
         assert_eq!(major_scale_of(TheoricalNote::from_str("C")), vec_of_slice_str(["C", "D", "E", "F", "G", "A", "B"]));
         assert_eq!(major_scale_of(TheoricalNote::from_str("C♯")), vec_of_slice_str(["C♯", "D♯", "E♯", "F♯", "G♯", "A♯", "B♯"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("C𝄪")), vec_of_slice_str(["C𝄪", "D𝄪", "E𝄪", "F𝄪", "G𝄪", "A𝄪", "B𝄪"]));
     }
 
     #[test]
-    fn test_build_major_scale() {
+    fn test_build_major_scale_d() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("D♭")), vec_of_slice_str(["D♭", "E♭", "F", "G♭", "A♭", "B♭", "C"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("D")), vec_of_slice_str(["D", "E", "F♯", "G", "A", "B", "C♯"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("D♯")), vec_of_slice_str(["D♯", "E♯", "F𝄪", "G♯", "A♯", "B♯", "C𝄪"]));
+    }
+
+    #[test]
+    fn test_build_major_scale_e() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("E♭")), vec_of_slice_str(["E♭", "F", "G", "A♭", "B♭", "C", "D"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("E")), vec_of_slice_str(["E", "F♯", "G♯", "A", "B", "C♯", "D♯"]));
         assert_eq!(major_scale_of(TheoricalNote::from_str("E♯")), vec_of_slice_str(["E♯", "F𝄪", "G𝄪", "A♯", "B♯", "C𝄪", "D𝄪"]));
+    }
+
+    #[test]
+    fn test_build_major_scale_f() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("F♭")), vec_of_slice_str(["F♭", "G♭", "A♭", "B𝄫", "C♭", "D♭", "E♭"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("F")), vec_of_slice_str(["F", "G", "A", "B♭", "C", "D", "E"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("F♯")), vec_of_slice_str(["F♯", "G♯", "A♯", "B", "C♯", "D♯", "E♯"]));
+    }
+
+    #[test]
+    fn test_build_major_scale_g() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("G♭")), vec_of_slice_str(["G♭", "A♭", "B♭", "C♭", "D♭", "E♭", "F"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("G")), vec_of_slice_str(["G", "A", "B", "C", "D", "E", "F♯"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("G♯")), vec_of_slice_str(["G♯", "A♯", "B♯", "C♯", "D♯", "E♯", "F𝄪"]));
+    }
+
+    #[test]
+    fn test_build_major_scale_a() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("A♭")), vec_of_slice_str(["A♭", "B♭", "C", "D♭", "E♭", "F", "G"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("A")), vec_of_slice_str(["A", "B", "C♯", "D", "E", "F♯", "G♯"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("A♯")), vec_of_slice_str(["A♯", "B♯", "C𝄪", "D♯", "E♯", "F𝄪", "G𝄪"]));
+    }
+
+    #[test]
+    fn test_build_major_scale_b() {
+        assert_eq!(major_scale_of(TheoricalNote::from_str("B♭")), vec_of_slice_str(["B♭", "C", "D", "E♭", "F", "G", "A"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("B")), vec_of_slice_str(["B", "C♯", "D♯", "E", "F♯", "G♯", "A♯"]));
+        assert_eq!(major_scale_of(TheoricalNote::from_str("B♯")), vec_of_slice_str(["B♯", "C𝄪", "D𝄪", "E♯", "F𝄪", "G𝄪", "A𝄪"]));
     }
 }
